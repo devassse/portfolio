e9f978c06c1d493fb535b680d744fcb94d67f0f7
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2023-03-27 10:49:03 +0200

    Responsive 971

--- v2/assets/js/scripts.js
+++ v2/assets/js/scripts.js
@@ -1,8 +1,26 @@
-
-
-
+// Date
 const anoRepo = document.getElementById('ano')
 
 const data = new Date()
 const ano = data.getFullYear()
 anoRepo.textContent = ano
+
+// Scroll Nav Effect
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active')
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+            })
+        }
+    })
+}
