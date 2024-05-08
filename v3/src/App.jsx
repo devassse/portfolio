43bab1e7d309ae860915671a271b3e265898eeae
Joao Devson Mucavel <joao.mucavel@ideiasdinamicas.com>
2024-05-08 10:50:27 +0200

    Portfolio v3

--- v3/src/App.jsx
+++ v3/src/App.jsx
@@ -1,28 +1,55 @@
-import { useState } from 'react'
-import { useTranslation } from 'react-i18next';
-import './App.css'
+import { Suspense } from "react";
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+import "./App.css";
+
+const translationEn = {welcome: "Welcome", title: "I am a Programmer!!", description: "With more than 8 years of experience in the professional market"};
+const translationPt = {welcome: "Seja Bem Vindo!", title: "Sou Programador!!", description: "Com mais de 8 anos de experiência no mercado profissional"};
+const translationJpRm = {welcome: "Irasshaimase", title: "Watashi wa Programma desu!!", description: "Purofesshonaru ichiba de 8-nen no keiken o motsu"};
+const translationJp = {welcome: "いらっしゃいませ", title: "私はですプログラム!!", description: "プロフェッショナル市場で8年の経験を持つ"};
+
+i18n.use(initReactI18next).init({
+  resources: {
+    en: { translation: translationEn },
+    pt: { translation: translationPt },
+    jprm: { translation: translationJpRm },
+    jp: { translation: translationJp },
+  },
+  lng: "en",
+  fallbacklng: "en",
+  interpolation: { escapeValue: false },
+});
 
 function App() {
-  const { t, i18n } = useTranslation();
-  const [count, setCount] = useState(0)
+  const { t } = useTranslation()
+
+  const onChange = (event) => {
+    i18n.changeLanguage(event.target.value)
+  };
 
   return (
     <>
-      
-      <h1 className="text-3xl font-bold underline">Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+      <Suspense fallback="Loading ...">
+        <div className="text-center">
+        <h1 className="welcome">{t('welcome')}</h1>
+        <h3 className="title">{t('title')}</h3>
+        
+        <p className="description">
+          {t('description')}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        </div>
+
+        <div className="lang-switcher">
+        <select name="language" onChange={onChange}>
+          <option value="en">English</option>
+          <option value="pt">Portuguese</option>
+          <option value="jprm">Japanese(Romage)</option>
+          <option value="jp">Japanese</option>
+        </select>
+        </div>
+      </Suspense>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
